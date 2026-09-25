2ce007922ab892410d0211be51d066b8cb16e3f3
RodrigoBorralho <159459102+RodrigoBorralho@users.noreply.github.com>
2026-09-25 13:34:49 +0100

    update exitcodes

---
+++ scripts/bash_first_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PATH="$PATH:/mnt/c/Users/rodri/Desktop/Desktop/projects/BashExplorativeProject/BashExplorativeProject/scripts"
+student="Sarah"
+echo "Hello ${student}"
--- scripts/exitcodes.sh
+++ scripts/exitcodes.sh
@@ -21,3 +21,15 @@ else
 fi   
 
 echo "The exit code for this script run is $?" 
+
+
+if [ -d $directory ]
+then 
+    echo "The directory $directory exists."
+    exit 0
+else
+
+    echo "The directory $directory doesn't exist."
+    exit 1    
+fi   
+
